caa59fdf2b6e5d95845d9ce7e19050f822a0cc96
m4k5 <git@m4k5.me>
2019-07-02 09:14:02 +0200

    added manage (again)

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
- def main():
+def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magazine_messenger_bot.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -17,5 +17,5 @@ import sys
     execute_from_command_line(sys.argv)
 
 
- if __name__ == '__main__':
+`if __name__ == '__main__':
     main()
